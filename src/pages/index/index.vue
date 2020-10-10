42c20e9673486692a89889ae5ca90779758de6b6
hhy666 <h18835005270@163.com>
2020-10-10 11:54:23 +0800

    初始化1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -20,14 +20,14 @@ export default {
     gs: gs
   },
   created(){
-    // 类型 zj 表示 中检一天 其他 表示具体公司w
+    // 类型 zj 表示 中检一天 其他 表示具体公司
     Taro.setStorageSync("showType", 'zj');
-    // 范围 0 集团 1 公司 3 区域公司 2 产品线w
+    // 范围 0 集团 1 公司 3 区域公司 2 产品线
     Taro.setStorageSync("showArea",0);
-    // 类型名称w
+    // 类型名称
     Taro.setStorageSync("showTypeName", '中检一天');
 
-    // 置顶w
+    // 置顶
     document.scrollingElement.scrollTop = 0;
   }
 }
